fdf826f594f0ed88845fb9efb1be1242c9470331
Rajia Rani <rajiarani.syal@gmail.com>
2024-10-07 00:36:23 -0500

    added update

--- server/app.js
+++ server/app.js
@@ -109,7 +109,7 @@ app.get("/country/:id", async (req, res) => {
 //Filter Route
 app.get("/university-filter/filtered-universities-list", universityController.getFilteredUniversities);
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
