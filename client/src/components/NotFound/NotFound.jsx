65d3230f3811388757f671921f54f6a930990032
Rajia Rani <rajiarani.syal@gmail.com>
2024-11-06 14:17:31 -0600

    added client 6 No

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import NotFoundImage from "../../assets/IMAGES/AllLogos/NotFound.jpg"; // Rename the image import
+import Navbar from "../NAVBAR/Navbar";
+import Footer from "../FOOTER/Footer";
+import "./NotFound.css";
 
 const NotFound = () => {
     return (
-        <div style={styles.container}>
-            <h1 style={styles.heading}>Oops! Page Not Found</h1>
-            <img src={NotFoundImage} alt="Cartoon Character Looking Lost" style={styles.image} />
-            <p style={styles.text}>
+        <>
+        <Navbar/>
+        <div className="notfound">
+            <h1>Oops! Page Not Found</h1>
+            <img src={NotFoundImage} alt="Cartoon Character Looking Lost"/>
+            <p>
                 It seems you're lost in the clouds! Why not go <Link to="/">back home</Link>?
             </p>
         </div>
+        <Footer/>
+        </>
     );
 };
 
-const styles = {
-    container: { textAlign: 'center', padding: '50px' },
-    heading: { color: '#333' },
-    image: { maxWidth: '500px', margin: '20px 0' },
-    text: { fontSize: '1.2em', color: '#666' }
-};
 
 export default NotFound;
 
