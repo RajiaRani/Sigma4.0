02c32ef21bbb73d34d1a893003d3848789a4bff7
Rajia Rani <rajiarani.syal@gmail.com>
2024-11-15 22:05:14 -0600

    added

--- client/src/UserPage/SignUp.jsx
+++ client/src/UserPage/SignUp.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/FOOTER/Footer";
 import Navbar from "../components/NAVBAR/Navbar";
 import "./SignUp.css";
+import { Button } from "@mui/material";
 import userpage from "../assets/IMAGES/AllLogos/user-page.jpg";
 import logo from "../assets/IMAGES/AllLogos/small-logo-transparent.png";
 
@@ -10,19 +11,25 @@ export default function SignUp() {
             <div>
                 <Navbar />
                 <div className="main-container">
-                    <h2>Sign Up To AbroadHub </h2>
+                    <h2>Signup to get started </h2>
+                    <p>Start your journey with AbroadHub.</p>
                     <div className="bg-image">
                         {/* <img src={ userpage} alt="user-page" /> */}
                         <img src={logo} alt="logo" className="logo-image" />
 
-                        <div className="user-form row">
-                            <div className="col col-sm-12 col-md-6">
+
+
+                        <div className="user-form ">
+                          
                                 <input type="email" placeholder="Email ID" className="form-input" />
                                 <input type="password" placeholder="Password" className="form-input" />
                                 <input type="text" placeholder="Name" className="form-input" />
-                            </div>
+                                <Button className="user-button">Sign Up</Button>
+                           
                         </div>
                     </div>
+
+
                 </div>
                 <Footer />
             </div>
