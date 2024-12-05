b36c91e6a824658d3d24398895fd006fdd0e3202
Rajia Rani <rajiarani.syal@gmail.com>
2024-12-05 11:11:48 -0600

    added 5 Dec

--- server/models/User.js
+++ server/models/User.js
@@ -1,31 +1,42 @@
+const { type } = require("express/lib/response");
 const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//     email:{
+//         type:String,
+//         required: true,
+//         unique:true
+//     },
+//     password:{
+//         type : String,
+//         required:true
+//     },
+//     name:{
+//         type : String,
+//         required:true
+//     },
+//     lastLogin:{
+//         type : Date,
+//         default: Date.now
+//     },
+//     isVerified:{
+//         type : Boolean,
+//         default: false
+//     },
+//     resetPasswordToken : String,
+//     resetPasswordExpiresAt : Date,
+//     verificationToken : String,
+//     verificationTokenExpiresAt: Date,
+// },{timestamps:true});
+
+
+
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
-        required: true,
-        unique:true
-    },
-    password:{
-        type : String,
-        required:true
-    },
-    name:{
-        type : String,
-        required:true
-    },
-    lastLogin:{
-        type : Date,
-        default: Date.now
-    },
-    isVerified:{
-        type : Boolean,
-        default: false
-    },
-    resetPasswordToken : String,
-    resetPasswordExpiresAt : Date,
-    verificationToken : String,
-    verificationTokenExpiresAt: Date,
-},{timestamps:true});
+        required: true
+    }
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports  = User;
