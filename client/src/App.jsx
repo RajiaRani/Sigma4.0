f911bcc4be2dbd0807cf905aff971fe7f3dff1e9
Rajia Rani <rajiarani.syal@gmail.com>
2024-12-07 18:03:53 -0600

    added 7 Dec

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,43 +8,42 @@ import Universities from "./pages/UNIVERSITY/Universities.jsx";
 import MSList from "./pages/UNIVERSITY/MSList.jsx";
 import UniversityDetails from "./pages/UNIVERSITY/UniversityDetails.jsx";
 import NotFound from "./components/NotFound/NotFound.jsx";
-import LoadingSpinner from "./components/LoadingSpiner/LoadingSpinner.jsx";
 import SignUp from "../src/UserPage/Passport/Signup.jsx";
 import Login from "../src/UserPage/Passport/Login.jsx";
-import EmailVerification from "./UserPage/Mailtrap/EmailVerification.jsx";
-import ResetPasswordPage from "./UserPage/Mailtrap/ResetPasswordPage.jsx";
-import ForgotPasswordPage from "./UserPage/Mailtrap/ForgotPasswordPage.jsx";
-import DashboardPage from "./UserPage/Mailtrap/DashboardPage.jsx";
-
+// import EmailVerification from "./UserPage/Mailtrap/EmailVerification.jsx";
+// import ResetPasswordPage from "./UserPage/Mailtrap/ResetPasswordPage.jsx";
+// import ForgotPasswordPage from "./UserPage/Mailtrap/ForgotPasswordPage.jsx";
+// import DashboardPage from "./UserPage/Mailtrap/DashboardPage.jsx";
+// import LoadingSpinner from "./components/LoadingSpiner/LoadingSpinner.jsx";
+// import { useAuthStore } from "./store/authStore.jsx";
+// import { useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { useAuthStore } from "./store/authStore.jsx";
-import { useEffect } from "react";
 
 
 // protect routes that require authentication
-const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, user } = useAuthStore();
+// const ProtectedRoute = ({ children }) => {
+//   const { isAuthenticated, user } = useAuthStore();
 
-  if (!isAuthenticated) {
-    return <Navigate to='/login' replace />;
-  }
+//   if (!isAuthenticated) {
+//     return <Navigate to='/login' replace />;
+//   }
 
-  if (!user.isVerified) {
-    return <Navigate to='/verify-email' replace />;
-  }
+//   if (!user.isVerified) {
+//     return <Navigate to='/verify-email' replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 // redirect authenticated users to the home page
-const RedirectAuthenticatedUser = ({ children }) => {
-  const { isAuthenticated, user } = useAuthStore();
+// const RedirectAuthenticatedUser = ({ children }) => {
+//   const { isAuthenticated, user } = useAuthStore();
 
-  if (isAuthenticated && user.isVerified) {
-    return <Navigate to='/' replace />;
-  }
+//   if (isAuthenticated && user.isVerified) {
+//     return <Navigate to='/' replace />;
+//   }
 
-  return children;
-};
+//   return children;
+// };
 
 
 function App() {
@@ -67,37 +66,8 @@ function App() {
             <Route path="/country/:id" element={<CountryDetails />} />
             <Route path="/universities/:id" element={<UniversityDetails />} />
             <Route path="/universities/MS-PG" element={<MSList />} />
-
-
-            <Route path="/auth/signup" element={
-              <RedirectAuthenticatedUser>
-                <SignUp />
-              </RedirectAuthenticatedUser>
-            } />
-
-            <Route path="/auth/login" element={<RedirectAuthenticatedUser>
-              <Login />
-            </RedirectAuthenticatedUser>} />
-
-            <Route
-					path='/auth/forgot-password'
-					element={
-						<RedirectAuthenticatedUser>
-							<ForgotPasswordPage />
-						</RedirectAuthenticatedUser>
-					}
-				/>
-
-				<Route
-					path='/auth/reset-password/:token'
-					element={
-						<RedirectAuthenticatedUser>
-							<ResetPasswordPage />
-						</RedirectAuthenticatedUser>
-					}
-				/>
-
-            <Route path="/auth/verify-email" element={<EmailVerification />} />
+            <Route path="/auth/signup" element={<SignUp />} />
+            <Route path="/auth/login" element={<Login /> } />
 
             {/* 404 Not Found Route */}
             <Route path="*" element={<NotFound />} />
